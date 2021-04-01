12053f3c03d3566bb3800e1ff22d0ab9142bda2b
farmerzhang1 <zhang2000dc@gmail.com>
2021-04-01 21:44:58 +0800

    alu io

--- lab1-ALU/src/main/scala/ALU/ALU.scala
+++ lab1-ALU/src/main/scala/ALU/ALU.scala
@@ -3,3 +3,24 @@ package ALU
 import chisel3._
 import chisel3.util._
 import chisel3.experimental._
+
+/*
+module alu #(
+parameter WIDTH = 32 //
+)(
+input [WIDTH-1] a, b, // operands
+input [2:0] f, // operator
+output [WIDTH-1:0] y, // result
+output z // zero sign
+);
+*/
+
+class ALU (val w: Int) extends Module {
+    val io = IO (new {
+        val a = Input (UInt(w.W)), // .W method for width
+        val b = Input (UInt(w.W)),
+        val op = Input (UInt(3.W)),
+        val res = Output (UInt(w.W)),
+        val z = Output (Bool())
+    })
+}
