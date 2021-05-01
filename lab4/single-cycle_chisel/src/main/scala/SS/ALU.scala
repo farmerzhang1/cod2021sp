5fa55394fe91080dc59f74e58426248338612156
farmerzhang1 <zhang2000dc@gmail.com>
2021-05-01 13:13:38 +0800

    开始写数据通路

--- lab4/single-cycle_chisel/src/main/scala/SS/ALU.scala
+++ lab4/single-cycle_chisel/src/main/scala/SS/ALU.scala
@@ -1,4 +1,5 @@
 package SingleCycle
+
 import chisel3._
 import chisel3.util._
 
--- lab4/single-cycle_chisel/src/main/scala/SS/ImmGen.scala
+++ lab4/single-cycle_chisel/src/main/scala/SS/ImmGen.scala
@@ -1,9 +1,23 @@
 package SingleCycle
 import chisel3._
+import chisel3.util._
+import Control._
 
 class ImmGen extends Module {
     val io = IO (new Bundle {
         val inst = Input(UInt(32.W))
+        val sel = Input(UInt(3.W))
         val imm = Output(UInt(32.W)) // seems like we need to sign-extend
     })
+    io.imm := MuxLookup (
+        io.sel,
+        0.S,
+        Seq(
+            IMM_X -> 0.S,
+            IMM_R -> 0.S,
+            IMM_I -> io.inst(31, 20).asSInt, // automatic sign-extension here
+            IMM_S -> Cat(io.inst(31, 25), io.inst(11, 7)).asSInt,
+            IMM_B -> Cat(io.inst(31), io.inst(7), io.inst(30, 25), io.inst(11, 8), 0.U(1.W)).asSInt, // !shift left by one bit
+            IMM_J -> Cat(io.inst(31), io.inst(19, 12), io.inst(20), io.inst(30, 21), 0.U(1.W)).asSInt // also shift left 1
+        )).asUInt
 }
